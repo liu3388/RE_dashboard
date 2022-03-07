c3a88ddcd9d516afb95de130f437607ae7a87d72
tai Liu <tl1688@yahoo.com>
2022-03-06 19:42:16 -0500

    dude

--- Github_dashboard.py
+++ Github_dashboard.py
@@ -156,6 +156,8 @@ with col1:
     
     df_chart1 = df_chart1b.append(df_chart1c)
     df_chart1['date2']=df_chart1['date'].astype(str)
+    df_chart1['median_listing_price'] = df_chart1['median_listing_price'].apply(lambda x: "${:.0f}k".format)
+
     
     
     fig = px.bar(df_chart1, x="date2", y="median_listing_price", 
