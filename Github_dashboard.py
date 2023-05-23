03197ce7f689fe4d2884fb7b98ecf2f92bafaba1
liu3388 <tl1688@yahoo.com>
2023-05-22 23:18:04 -0400

    Update Github_dashboard.py

--- Github_dashboard.py
+++ Github_dashboard.py
@@ -157,7 +157,7 @@ df_rent_chart = df_rent_chart[['2 bedroom', '3 bedroom', '4 bedroom', 'year']]
 df_rent_chart = df_rent_chart[df_rent_chart['year'].isin(['2018', '2022'])]
 
 #add rent columns to df
-br2_rent = df_rent_chart['2 bedroom'].iloc[-1]
+br2_rent = df_rent_chart['2 bedroom'].iloc[0]
 br3_rent = df_rent_chart['3 bedroom'].iloc[-1]
 br4_rent = df_rent_chart['4 bedroom'].iloc[-1]    
 
